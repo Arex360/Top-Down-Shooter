b5bf5da168fa300e2f5639c2c8ca987f3b4992fc
Arex360 <49303647+Arex360@users.noreply.github.com>
2022-12-31 23:15:21 -0800

    update

---
+++ Assets/Scripts/CameraRender.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class xEnumDefine//Enumeration definition class
+{
+    public enum TeamFlag
+    {
+        Invalid = -1,
+        Team_0 = 0,
+        Team_1 = 1,
+    }
+}
+
+public class CustomColorData_0
+{
+    public static Color Color_Team_0 = new Color(93 / 255.0f, 25 / 255.0f, 231 / 255.0f);
+    public static Color Color_Team_1 = new Color(233 / 255.0f, 106 / 255.0f, 173 / 255.0f);
+}
+
+public class CameraRender : MonoBehaviour
+{
+    public static CameraRender Instance;
+
+    public class HitObj
+    {
+        public GameObject obj;
+        public Vector2 texcoord;
+        public Color color;
+        public int coltype;
+        public string decalname = "Gun_H100";
+        public float decalwidth = 1.0f;
+        public float decalheight = 1.0f;
+        public float decalrot = 0.0f;
+    };
+
+    public List<HitObj> M_StaticObjArray
+    {
+        set
+        {
+            mStaticObjArray = value;
+        }
+
+        get
+        {
+            return mStaticObjArray;
+        }
+    }
+
+    private List<HitObj> mStaticObjArray = new List<HitObj>();
+
+    private Material mQuadMaterial;
+
+    public bool M_OnceRender
+    {
+        get
+        {
+            return mOnceRender;
+        }
+        set
+        {
+            mOnceRender = value;
+        }
+    }
+
+    private bool mOnceRender = false;
+    public  bool M_OpenOnceRender
+    {
+        get
+        {
+            return mOpenOnceRender;
+        }
+        set
+        {
+            mOpenOnceRender=value;
+        }
+    }
+    private bool mOpenOnceRender = false;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.OpenGLES2 ||
+            SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.OpenGLES3)
+        {
+            mQuadMaterial = new Material(Shader.Find("SpraySoldier/Function/MobileDrawDecal"));
+        }
+        else
+        {
+            mQuadMaterial = new Material(Shader.Find("SpraySoldier/Function/DrawDecal"));
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    void OnPostRender()
+    {
+        for (int i = 0; i < mStaticObjArray.Count; i++ )
+        {
+            HitObj obj = mStaticObjArray[i];
+            if (obj == null)
+                continue;
+
+            StaticObj_Render render = obj.obj.GetComponent<StaticObj_Render>();
+            if (render == null)
+                continue;
+
+            render.Render(obj.texcoord, mQuadMaterial, obj.color, obj.coltype, obj.decalname, obj.decalwidth, obj.decalheight, obj.decalrot);
+        }
+
+        mStaticObjArray.Clear();
+    }
+}
---
+++ Assets/Scripts/ThirdPersonCam.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    #region Constant
+
+    public const string INPUT_MOUSE_SCROLLWHEEL = "Mouse ScrollWheel";
+    public const string ERROR_UN_BINDCAM = "ThirdPersonCam脚本没有绑定摄像机!"; //chinese text says "The script does not bind the camera"
+    public const string ERROR_UN_PLAYER = "ThirdPersonCam脚本没有指定玩家"; //chinese text says "The script does not specify a player"
+
+    /// The basic direction of the camera
+    private Vector3 CamBaseAxis = Vector3.back;
+
+    /// The distance from the intersection of the camera and the collision body to the observation point of the camera
+    private float CollisionReturnDis = 0.5f;
+
+    #endregion
+
+    #region Variable
+
+    /// Video camera
+    private Transform mCamera;
+
+    /// Player transform
+    public Transform mPlayer;
+
+    /// Character center point offset
+    public Vector3 mPivotOffset = new Vector3(0.0f, 1.0f, 0.0f);
+
+    /// Horizontal aiming speed
+    public float mHorizontalAimingSpeed = 400.0f;
+
+    /// Vertical aiming speed
+    public float mVerticalAimingSpeed = 400.0f;
+
+    /// Maximum vertical angle
+    public float mMaxVerticalAngle = 30.0f;
+
+    /// Minimum vertical angle
+    public float mMinVerticalAngle = -60.0f;
+
+    /// The maximum value of the magnification of the base camera shift
+    public float mMaxDistance = 2.0f;
+
+    /// The minimum value of the magnification of the base camera shift
+    public float mMinDistance = 1.0f;
+
+    /// The speed of lens advance
+    public float mZoomSpeed = 5.0f;
+
+    /// The angle of horizontal rotation
+    private float mAngleH = 0.0f;
+
+    /// Vertical rotation angle
+    private float mAngleV = -30.0f;
+
+    /// Base camera shift magnification
+    private float mDistance = 0.0f;
+
+    #endregion
+
+    #region Built-in function
+
+    void Awake()
+    {
+        mCamera = GetComponent<Camera>().transform;
+        mDistance = (mMinDistance + mMaxDistance) * 0.5f;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mCamera == null)
+        {
+            Debug.LogError(ERROR_UN_BINDCAM);
+            return;
+        }
+
+        if (mPlayer == null)
+        {
+            Debug.LogError(ERROR_UN_PLAYER);
+            return;
+        }
+
+        float mousex = Input.GetAxis("Mouse X");
+        float mousey = Input.GetAxis("Mouse Y");
+        float zoom = Input.GetAxis("Mouse ScrollWheel");
+        mAngleH += (Mathf.Clamp(mousex, -1.0f, 1.0f) * mHorizontalAimingSpeed);
+        mAngleV += (Mathf.Clamp(mousey, -1.0f, 1.0f) * mVerticalAimingSpeed);
+        mDistance -= zoom * mZoomSpeed;
+
+        mAngleV = Mathf.Clamp(mAngleV, mMinVerticalAngle, mMaxVerticalAngle);
+        mDistance = Mathf.Clamp(mDistance, mMinDistance, mMaxDistance);
+
+        Quaternion animRotation = Quaternion.Euler(-mAngleV, mAngleH, 0.0f);
+        Quaternion camYRotation = Quaternion.Euler(0.0f, mAngleH, 0.0f);
+        mCamera.rotation = animRotation;
+
+        Vector3 lookatpos = mPlayer.position + camYRotation * mPivotOffset;
+        Vector3 camdir = animRotation * CamBaseAxis;
+        camdir.Normalize();
+        mCamera.position = lookatpos + camdir * mDistance;
+
+        // Calculate camera points after collision
+        RaycastHit rayhit;
+        bool hit = Physics.Raycast(lookatpos, camdir, out rayhit, mDistance);
+        if (hit)
+        {
+            // Block character collision
+            bool charcol = rayhit.collider as CharacterController;
+            if (!charcol)
+            {
+                Vector3 modifypos = rayhit.normal * CollisionReturnDis * 2.0f;
+                mCamera.position = rayhit.point + modifypos;
+
+                // The distance correction is within the range (1, to avoid the camera interspersed into the character)
+                float distance = Vector3.Distance(mCamera.position, lookatpos);
+                distance = Mathf.Clamp(distance, mMinDistance, mMaxDistance);
+                mCamera.position = lookatpos + camdir * distance + modifypos;
+            }
+        }
+    }
+
+    void OnDestroy()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+
+    }
+
+    #endregion
+}
